5a6478921f8823fa66c9175739ae5e37ed9fd201
jianhengqiu <kinhunt@gmail.com>
2017-05-30 01:49:13 +0800

    change port

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+    res.send('Hello World!')
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(process.env.PORT, function () {
+    console.log('Example app listening on port ' + process.env.PORT)
 })
