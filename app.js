2271399781fc8a1fbc62cb63933ecf77be1d3567
jianhengqiu <kinhunt@gmail.com>
2017-05-30 02:43:14 +0800

    get youtube info

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const app = express()
 
-app.get('/', function (req, res) {
-    res.send('Hello World!')
+const fs = require('fs');
+const ytdl = require('ytdl-core');
+
+
+
+app.get('/:vid', function (req, res) {
+    ytdl.getInfo('http://www.youtube.com/watch?v=' + req.params.id, function (err, info) {
+        res.json(info)
+    })
 })
 
 app.listen(process.env.PORT, function () {
+
     console.log('Example app listening on port ' + process.env.PORT)
 })
