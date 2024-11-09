2490776159270779ebbb3752219f52a2f7880169
HomeDev68 <180551684+HomeDev68@users.noreply.github.com>
2024-11-09 01:13:18 +0100

    Add error handling for missing `formats` property in `/youtube/:vid` route
    * Check if `formats` property exists before accessing it
    * Return a 500 status with an error message if `formats` is missing or empty

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 const https = require('https'),
     express = require('express'),
     app = express(),
@@ -14,8 +13,18 @@ var proxy = httpProxy.createProxyServer({
     agent: https.globalAgent
 }); // See (†)
 
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'instructions.html'));
+});
+
 app.get('/youtube/:vid', function (req, res) {
     ytdl.getInfo('http://www.youtube.com/watch?v=' + req.params.vid, function (err, info) {
+        if (err) {
+            return res.status(500).send('Error retrieving video info');
+        }
+        if (!info.formats || info.formats.length === 0) {
+            return res.status(500).send('No video formats found');
+        }
         let downloadUrl = info.formats[0].url
         var result = url.parse(downloadUrl);
         let host = result.hostname.replace(".googlevideo.com", '')
@@ -31,11 +40,11 @@ app.get('/stream/youtube/:host/*', function (req, res) {
         target: 'https://' + host, headers: {
             host: host
         }
+    }, function (err) {
+        res.status(500).send('Error proxying request');
     });
 })
 
-
 app.listen(process.env.PORT, function () {
-
     console.log('Example app listening on port ' + process.env.PORT)
 })
